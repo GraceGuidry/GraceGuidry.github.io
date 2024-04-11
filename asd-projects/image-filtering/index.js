c72b8cf2e596d70c68a1d45e91828ce3f4e7f199
GraceGuidry <gng3339917@lpssonline.com>
2024-04-11 15:02:37 +0000

    saving walker

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,9 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilterNotBackground(reddify)
+  applyFilterNoBackground(reddify)
   applyFilter(decreaseBlue)
-  applyFilterNotBackground(increaseGreenByBlue)
+  applyFilterNoBackground(increaseGreenByBlue)
   
 
   // do not change the below line of code
@@ -35,20 +35,20 @@ function applyAndRender() {
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 function applyFilter (filterFunction){
-
+  
 }
 
 // TODO 7: Create the applyFilterNoBackground function
-function applyFilterNotBackground (filterFunction){
+function applyFilterNoBackground (filterFunction){
   var backgroundColor = image[0][0]
   for (var i = 0; i < image.length; i++){
     for (var j = 0; j < image[i].length; j++) {
       if (image [i][j] !== backgroundColor){
         var rgbString = image[i][j]
-                var rgbNumber = rgbStringToArray(rgbString)
-                filterFunction(rgbNumbers)
-                rgbString = rgbArrayToString(rgbNumbers)
-                image[i][j] = rgbString
+        var rgbNumber = rgbStringToArray(rgbString)
+        filterFunction(rgbNumbers)
+        rgbString = rgbArrayToString(rgbNumbers)
+        image[i][j] = rgbString
       }
     }
   }
--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -11,12 +11,24 @@ function runProgram(){
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
+  var KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  }
   // Game Item Objects
-
+  var walker = {
+    posX: 0,
+    posY: 0,
+    speedX: 0,
+    speedY: 0,
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);                         
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +39,54 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem()
+    redrawGameItem()
+    wallCollision()
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown (event) {
+    if (event.which === KEY.LEFT){
+      walker.speedX = -5
+    } else if (event.which === KEY.RIGHT){
+      walker.speedX = 5
+    } else if (event.which === KEY.UP){
+      walker.speedY = -5
+    } else if (event.which === KEY.DOWN){
+      walker.speedY = 5
+    }
   }
+
+function handleKeyUp (event){
+  walker.speedX = 0;
+  walker.speedY = 0;
+}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function repositionGameItem (){
+    walker.posX += walker.speedX;
+    walker.posY += walker.speedY;
+  }
+
+  function redrawGameItem (){
+    $("#walker").css("left", walker.posX);
+    $("#walker").css("up", walker.posY);
+  }
+
+  function wallCollision (){
+    if (walker.posX >= $("#board").width() || walker.posX <= 0){
+      walker.posX -= walker.speedX;
+    } else if (walker.posY >= $("#board").height() || walker.posY <= 0){
+      walker.posY -= walker.speedY;
+    }
+  }
+
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
